7d6f38080de528a6212441d26f69a8945bb08b42
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-04-05 01:18:56 -0500

    Parser rules defined

--- rust_lex.py
+++ rust_lex.py
@@ -72,7 +72,7 @@ t_RBCKT = r'}'
 t_SEMCL = r';'
 
 # -----------------------------------------------------------------------------
-# Rules
+# Lexer Rules
 # -----------------------------------------------------------------------------
 
 # Rule for ID's
@@ -128,16 +128,188 @@ def find_column(input,token):
 import ply.lex as lex
 lexer = lex.lex(debug=0)
 
-# Keep running
+# Import yacc
+import ply.yacc as yacc
+parser = yacc.yacc()
 
-def lexer(data):
-    lex.input(data)
+# -----------------------------------------------------------------------------
+# Parsing Rules
+# -----------------------------------------------------------------------------
 
-    while True:
-        tok = lex.token()
-        print(tok)
-        if not tok:
-            break
+"""
+precedence = (
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('right', 'UMINUS'),
+)
+"""
+
+# Dictionary of names
+names = {}
+
+# Rule to run program
+def p_program(p):
+    """program : declarationList
+    """
+    pass
+
+# Rule for general statement
+def p_general(p):
+    """general : stmt
+               | stmt general
+    """
+    pass
+
+# Rule to implement statements
+def p_stmt(p):
+    """stmt : expressionStmt
+            | declaration
+            | selectionStmt
+            | iterationStmt
+            | inputStmt
+            | outputStmt
+            | returnStmt
+    """
+    pass
+
+# Rule to implement several declarations
+def p_declarationList(p):
+    """declarationList : declarationList declaration
+                       | declaration
+    """
+    pass
+
+# Rule to implement single declaration
+def p_declaration(p):
+    """declaration : varDeclaration
+                   | constDeclaration
+                   | staticDeclaration
+    """
+    pass
+
+# Rule to implement variable declaration
+def p_varDeclaration(p):
+    """varDeclaration : LET IDVAR EQUAL IDVAR SEMCL
+                      | LET IDVAR EQUAL INTVR SEMCL
+                      | LET IDVAR EQUAL expressionStmt
+    """
+    pass
+
+# Rule to implement constant declaration
+def p_constDeclaration(p):
+    """constDeclaration : CONST IDVAR EQUAL IDVAR SEMCL
+    """
+    pass
+
+# Rule to implement static declaration
+def p_staticDeclaration(p):
+    """staticDeclaration : STATIC IDVAR EQUAL IDVAR SEMCL
+    """
+    pass
+
+# Rule to implement expressions
+def p_expressionStmt(p):
+    """expressionStmt : expression
+    """
+    pass
+
+# Rule to implement iterations
+def p_iterationStmt(p):
+    """iterationStmt : FOR IDVAR IN expression
+    """
+    pass
+
+# Rule to implement iterations
+def p_selectionStmt(p):
+    """iterationStmt : FOR IDVAR IN expression
+    """
+    pass
+
+# Rule for general expression
+def p_expression(p):
+    """expression   : expression OROPE andExpr
+                    | andExpr
+    """
+    pass
+
+# Rule for AND expression
+def p_andExpr(p):
+    """andExpr  : andExpr ANDOP relExpr
+                | relExpr
+    """
+    pass
+
+# Rule for regular expression
+def p_rel_expr(p):
+    """relExpr  : sumExpr relop sumExpr
+    """
+    pass
+
+# Rule for operators
+def p_relop(p):
+    """relop    : LESST
+                | LSSEQ
+                | GREAT
+                | GRTEQ
+                | EQUAL
+                | UNEQL
+                | EQLTO
+    """
+    pass
+
+# Rule for sum expression
+def p_sumExpr(p):
+    """sumExpr  : sumExpr sumOp term
+                | term
+    """
+    pass
+
+# Rule for sum operator
+def p_sumOp(p):
+    """sumOp    : ADDOP
+                | SUBOP
+    """
+    pass
+
+# Rule for terms
+def p_term(p):
+    """term : term mulOp number
+            | number
+    """
+    pass
+
+# Rule to implement numbers
+def p_number(p):
+    """number : INTVR
+    """
+    pass
+
+# Rule for multiplication and division operator
+def p_mulop(p):
+    """mulop    : DEROP
+                | QUOOP
+    """
+    pass
+
+# Rule for inputs
+def p_input_stmt(p):
+    """inputStmt : STDIN OPENP stmt CLOSP
+    """
+    pass
+
+# Rule for outputs
+def p_output_stmt(p):
+    """outputStmt : PRINTLN OPENP stmt CLOSP
+    """
+    pass
+
+# Rule for booleans
+def p_booleans(p):
+    """booleans : LET IDVAR EQUAL TRUE
+                | LET IDVAR EQUAL FALSE
+    """
+    pass
+
 
 # -----------------------------------------------------------------------------
 # Tests
