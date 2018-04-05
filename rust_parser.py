a732d443f95bb24e1f1f4bd94a470d24c3e559a7
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-04-05 10:44:13 -0500

    Parser fixing

--- rust_parser.py
+++ rust_parser.py
@@ -49,66 +49,83 @@ precedence = (
 names = {}
 
 def p_foo(p):
-    """foo : program
+    """
+    foo : program
     """
     pass
 
 # Rule to run program
 def p_program(p):
-    """program : declarationList
     """
-    pass
+    program : declarationList
+    """
+    p[0] = Node("program", None, None, [p[1]])
+    p[0].debug()
+    print("\n")
 
 # Rule for general statement
 def p_general(p):
-    """general : stmt
-               | stmt general
+    """
+    general : stmt
+            | stmt general
     """
     pass
 
 # Rule to implement statements
 def p_stmt(p):
-    """stmt : expressionStmt
-            | declaration
-            | selectionStmt
-            | iterationStmt
-            | inputStmt
-            | outputStmt
+    """
+    stmt : expressionStmt
+         | declaration
+         | selectionStmt
+         | iterationStmt
+         | inputStmt
+         | outputStmt
     """
     pass
 
 # Rule to implement several declarations
 def p_declarationList(p):
-    """declarationList : declarationList declaration
-                       | declaration
     """
-    pass
+    declarationList : declaration
+                    | declaration declarationList
+    """
+    if len(p) == 2:
+        p[0] = Node("declaration-list", None, None, [p[1]])
+    else:
+        p[0] = Node("declaration-list", None, None, [p[1], p[2]])
 
 # Rule to implement single declaration
 def p_declaration(p):
-    """declaration : varDeclaration
-                   | constDeclaration
-                   | staticDeclaration
     """
-    pass
+    declaration : varDeclaration
+                | constDeclaration
+                | staticDeclaration
+    """
+    p[0] = Node("declaration", None, None, [p[1]])
 
 # Rule to implement variable declaration
 def p_varDeclaration(p):
-    """varDeclaration : LET IDVAR EQUAL IDVAR
-                      | LET IDVAR EQUAL INTVR
-                      | LET IDVAR EQUAL expressionStmt
     """
-    pass
+    varDeclaration : LET IDVAR EQUAL IDVAR SEMCL
+                   | LET IDVAR EQUAL INTVR SEMCL
+                   | LET IDVAR EQUAL expressionStmt SEMCL
+    """
+    if len(p) == 6:
+        p[0] = Node('varDeclaration', [p[2], p[2]], None, None)
+    else:
+        p[0] = Node('varDeclaration', [p[2]], None, None)
 
 # Rule to implement constant declaration
 def p_constDeclaration(p):
-    """constDeclaration : CONST IDVAR EQUAL IDVAR SEMCL
+    """
+    constDeclaration : CONST IDVAR EQUAL IDVAR SEMCL
     """
     pass
 
 # Rule to implement static declaration
 def p_staticDeclaration(p):
-    """staticDeclaration : STATIC IDVAR EQUAL IDVAR SEMCL
+    """
+    staticDeclaration : STATIC IDVAR EQUAL IDVAR SEMCL
     """
     pass
 
@@ -120,106 +137,120 @@ def p_expressionStmt(p):
 
 # Rule to implement iterations
 def p_iterationStmt(p):
-    """iterationStmt : FOR IDVAR IN expression
-                     | WHILE expression EQLTO general
-                     | WHILE expression LESST general
-                     | WHILE expression GREAT general
+    """
+    iterationStmt : FOR IDVAR IN expression
+                  | WHILE expression EQLTO general
+                  | WHILE expression LESST general
+                  | WHILE expression GREAT general
     """
     pass
 
 # Rule to implement iterations
 def p_selectionStmt(p):
-    """selectionStmt : IF expression EQUAL general
-                     | IF expression EQUAL general ELSE
+    """
+    selectionStmt : IF expression EQUAL general
+                  | IF expression EQUAL general ELSE
     """
     pass
 
 # Rule for general expression
 def p_expression(p):
-    """expression   : expression OROPE andExpr
-                    | andExpr
+    """
+    expression : expression OROPE andExpr
+               | andExpr
     """
     pass
 
 # Rule for AND expression
 def p_andExpr(p):
-    """andExpr  : andExpr ANDOP relExpr
-                | relExpr
+    """
+    andExpr : andExpr ANDOP relExpr
+            | relExpr
     """
     pass
 
 # Rule for regular expression
 def p_rel_expr(p):
-    """relExpr  : sumExpr relop sumExpr
+    """
+    relExpr : sumExpr relop sumExpr
     """
     pass
 
 # Rule for operators
 def p_relop(p):
-    """relop    : LESST
-                | LSSEQ
-                | GREAT
-                | GRTEQ
-                | EQUAL
-                | UNEQL
-                | EQLTO
+    """
+    relop : LESST
+          | LSSEQ
+          | GREAT
+          | GRTEQ
+          | EQUAL
+          | UNEQL
+          | EQLTO
     """
     pass
 
 # Rule for sum expression
 def p_sumExpr(p):
-    """sumExpr  : sumExpr sumOp term
-                | term
+    """
+    sumExpr : sumExpr sumOp term
+            | term
     """
     pass
 
 # Rule for sum operator
 def p_sumOp(p):
-    """sumOp    : ADDOP
-                | SUBOP
+    """
+    sumOp : ADDOP
+          | SUBOP
     """
     pass
 
 # Rule for terms
 def p_term(p):
-    """term : term mulOp number
-            | number
+    """
+    term : term mulOp number
+         | number
     """
     pass
 
 # Rule to implement numbers
 def p_number(p):
-    """number : INTVR
+    """
+    number : INTVR
     """
     pass
 
 # Rule for multiplication and division operator
 def p_mulOp(p):
-    """mulOp    : DEROP
-                | QUOOP
+    """
+    mulOp : DEROP
+          | QUOOP
     """
     pass
 
 # Rule for inputs
 def p_input_stmt(p):
-    """inputStmt : STDIN OPENP stmt CLOSP
+    """
+    inputStmt : STDIN OPENP stmt CLOSP
     """
     pass
 
 # Rule for outputs
 def p_output_stmt(p):
-    """outputStmt : PRINTLN OPENP stmt CLOSP
+    """
+    outputStmt : PRINTLN OPENP stmt CLOSP
     """
     pass
 
 # Rule for booleans
 def p_booleans(p):
-    """booleans : LET IDVAR EQUAL TRUE
-                | LET IDVAR EQUAL FALSE
+    """
+    booleans : LET IDVAR EQUAL TRUE
+             | LET IDVAR EQUAL FALSE
     """
     pass
 
-# Rule for Syntax error
+# Rule for Catastrophic error
 def p_error(p):
     if p:
         print("Syntax error at '%s'" % p.value)
