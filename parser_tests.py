5e137482b4452dff42440289d8913d402286cf5d
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-04-05 02:30:59 -0500

    Lexer Tests

--- parser_tests.py
+++ parser_tests.py
@@ -2,13 +2,18 @@ from parser import *
 
 # Success
 # Variable definition
+'''
 let FIVE = 5;
+'''
 
 # Constants definition
+'''
 static NINE = 9;
 const TEN = 10;
+'''
 
 # Loop and conditional
+'''
 for var in expression {
     code
 }
@@ -18,13 +23,17 @@ if n < 10 {
 else {
  code
 }
+'''
 
 # Input and output
+'''
 for line in stdin {
 }
 println!("I just printed this!");
+'''
 
 # All the rules
+'''
 while FIVE > 5:
     stdin()
     println!("Yes")
@@ -33,25 +42,38 @@ while FIVE > 5:
         print("FIVE is now 3")
     else:
         print("Value is not 3");
+'''
 
 # Failure
 # Variable definition on the wrong place
+'''
 "chris" let = crsc
+'''
 
 # Using a string in a not allowed way
+'''
 "Esto es un string" = si;
+'''
 
 # Loop with wrong wrammar
+'''
 while:
     print("No")
+'''
 
 # Incorrect inputs but with success for now
+'''
 x = 5
 x = "String"
+'''
 
 # Method using a class that does not contain such a method
+'''
 x.method1().method2()
+'''
 
 # Variable outside the scope
+'''
 def function1(): variable2 = 2; variable2 x;
 variable2;
+'''
